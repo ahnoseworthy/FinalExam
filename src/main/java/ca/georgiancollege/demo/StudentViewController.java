bbe7da02e35879b980f7f9471c9a1d7cd8825d6d
ahnoseworthy <144746886+ahnoseworthy@users.noreply.github.com>
2024-06-20 16:58:23 -0400

    Question 1 complete

---
+++ src/main/java/ca/georgiancollege/demo/StudentViewController.java
@@ -0,0 +1,60 @@
+package ca.georgiancollege.demo;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+
+
+public class StudentViewController  {
+
+    @FXML
+    private TableView<Student> tableView;
+
+    @FXML
+    private TableColumn<Student, Integer> studentNumCol;
+
+    @FXML
+    private TableColumn<Student, String> firstNameCol;
+
+    @FXML
+    private TableColumn<Student, String> lastNameCol;
+
+    @FXML
+    private TableColumn<Student, String> telephoneCol;
+
+    @FXML
+    private TableColumn<Student, String> addressCol;
+
+    @FXML
+    private TableColumn<Student, String> provinceCol;
+
+    @FXML
+    private TableColumn<Student, Integer> avgGradeCol;
+
+    @FXML
+    private TableColumn<Student, String> majorCol;
+
+    @FXML
+    private CheckBox ontarioCheckBox;
+
+    @FXML
+    private Label numOfStudentsLabel;
+
+    @FXML
+    private CheckBox honourRollCheckBox;
+
+    @FXML
+    private ComboBox<String> areaCodeComboBox;
+    private String collectionOfStrings;
+
+    @FXML
+    private void applyFilter()  {
+    }
+
+    @FXML
+    void initialize() {
+        areaCodeComboBox.getItems().add("what ever");
+        areaCodeComboBox.getItems().addAll(collectionOfStrings);
+        areaCodeComboBox.getSelectionModel().getSelectedItem();
+        
+    }
+}
